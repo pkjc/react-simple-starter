e15757bc1b30a0db2716bbc62d9e9550706d1565
pkjc <mepankaj@live.in>
2021-09-05 11:50:42 -0400

    Initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>React Simple Starter</h1>
+      <h2>[Work in Progress]</h2>
+      <ul>
+        <li>Material-UI for UI components</li>
+        <li>React Router for routing</li>
+        <li>Easy Peasy for state</li>
+        <li>Axios for API communication</li>
+        <li>React Hook Form for forms</li>
+        <li>Code splitting</li>
+        <li>ESLint</li>
+        <li>Prettier</li>
+        <li>Simple project structure</li>
+        <li>Always up-to-date dependencies</li>
+        <li>Usage examples</li>
+      </ul>
+    </div>
+  );
+}
